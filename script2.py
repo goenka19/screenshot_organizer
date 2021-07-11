dcecc937761486740e865d813f49359a1dea1b09
Ujjwal Goenka <46502186+goenka19@users.noreply.github.com>
2021-07-11 21:16:17 +0530

    updated for the new semester

--- script2.py
+++ script2.py
@@ -6,24 +6,22 @@ from datetime import date, datetime, time
 home_dir= "/Users/ujjwalgoenka/Desktop/Screenshots"
 
 #directory path to the folders where screenshots need to be transferred
-dir_one = "/Users/ujjwalgoenka/Desktop/Semester 2/Managment Accounting/screenshots" #MA
-dir_two = "/Users/ujjwalgoenka/Desktop/Semester 2/Corporate Finance/new"            #CF
-dir_three = "/Users/ujjwalgoenka/Desktop/Semester 2/QT/screenshots"                 #QT
-dir_four = "/Users/ujjwalgoenka/Desktop/Semester 2/Macro/screenshots"               #Macro
-dir_five = "/Users/ujjwalgoenka/Desktop/Semester 2/POM/screenshots"                 #POM
-dir_six = "/Users/ujjwalgoenka/Desktop/Semester 2/IT/screenshots"                   #IT
+dir_one = "/Users/ujjwalgoenka/Desktop/Semester 3/CF"                               #CF
+dir_two = "/Users/ujjwalgoenka/Desktop/Semester 3/OBHRM"                            #OBHRM
+dir_three = "/Users/ujjwalgoenka/Desktop/Semester 3/OR"                             #OR
+dir_four = "/Users/ujjwalgoenka/Desktop/Semester 3/BPSM"                            #BPSM
+dir_five = "/Users/ujjwalgoenka/Desktop/Semester 3/DM"                              #DM
+dir_six = "/Users/ujjwalgoenka/Desktop/Semester 3/FSABV"                            #FSABV
 
 
 #time to compare
-class_one_start = time(12,30,00)
-class_one_end = time(14,30,00)
-class_two_start = time(15,00,00)
-class_two_end = time(17,00,00)
-class_three_start = time(10,00,00)
-class_three_end = time(12,00,00)
+class_one_start = time(7,30,00)
+class_one_end = time(9,30,00)
+class_two_start = time(10,00,00)
+class_two_end = time(12,00,00)
 
 #date after it starts comparing 
-start_date = date(2021,1,10)
+start_date = date(2021,7,10)
 
 #listing the content of home directory
 files = os.listdir(home_dir)
@@ -43,11 +41,9 @@ for file in files:
         #print("Time Not Bound")
         continue
     if class_one_start < only_time < class_one_end:
-        _subject_map = {'Monday': dir_one, 'Tuesday': dir_three, 'Wednesday': dir_four, 'Thursday': dir_three, 'Friday': dir_four, 'Saturday':dir_five}    
+        _subject_map = {'Monday': dir_one, 'Tuesday': dir_three, 'Wednesday': dir_four, 'Thursday': dir_six, 'Friday': dir_three, 'Saturday':dir_five}    
     elif class_two_start < only_time < class_two_end:
-        _subject_map = {'Monday': dir_two, 'Tuesday': dir_one, 'Wednesday': dir_five, 'Saturday': dir_six, 'Friday': dir_two}
-    elif class_three_start < only_time < class_three_end:
-        _subject_map = {'Thursday' : dir_six}  
+        _subject_map = {'Monday': dir_two, 'Tuesday': dir_four, 'Wednesday': dir_five,'Thursday':dir_one, 'Friday': dir_six, 'Saturday': dir_two}
     else:
        #print("Error")
        continue
